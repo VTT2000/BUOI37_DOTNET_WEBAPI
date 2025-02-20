d57ca301540ec49bc3059aa192e602587924c5be
VuuThuan <thuancmag2000@gmail.com>
2025-02-20 18:55:09 +0700

    buoi37_web_api

--- web_api/obj/Debug/net8.0/web_api.AssemblyInfo.cs
+++ web_api/obj/Debug/net8.0/web_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de48258c124e3f3ee38c75e4f307746429f8b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
